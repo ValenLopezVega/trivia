d6da0a1e35732c734e3fee6f7aa0c5c6fcb7d870
valen2004vega <valen2004vega@gmail.com>
2024-05-21 16:06:03 -0500

    feat: exercise one

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ function greet() {
   let selected1 = document.querySelector('input[type=radio][name=question1]:checked');
 
   let selected2 = document.querySelector('input[type=radio][name=question2]:checked');
-  alert('Usted seleccionó ' + selected1.value + ' y ' + selected2.value);
+
+  alert(`Usted seleccionó ${selected1.value} y ${selected2.value}`);
 }
-
-
