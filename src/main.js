cf57eb543b96abf9bf42b07c9b01049ca57e1bde
valen2004vega <valen2004vega@gmail.com>
2024-06-18 15:57:33 -0500

    feat: created welcome message

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ function greet() {
 
   alert(`Usted seleccionó ${selected1.value} y ${selected2.value}`);
 }
+
+const playGame = () => {
+  let name = document.getElementById('name').value;
+  document.getElementById('questions').style.display = 'block';
+  document.getElementById('intro').style.display = 'none';
+  document.getElementById('welcomeMsg').textContent = `Hola ${name}`;
+}
+
+const restart = () => {
+  document.getElementById('questions').style.display = 'none';
+  document.getElementById('intro').style.display = 'block';
+}
+
+
+
