95b895d79783277bc953011d97c15aba56b4d855
valen2004vega <valen2004vega@gmail.com>
2024-11-29 14:30:49 -0500

    feat: pwa config files

---
+++ src/serviceWorker.js
@@ -0,0 +1,29 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable arrow-parens */
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+const trivia = 'trivia-site-v1';
+const assets = [
+  '/',
+  'index.html',
+  'main.js',
+  'style.css',
+  'src/img/favicon.ico'
+]
+
+// eslint-disable-next-line arrow-parens
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(trivia).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    }),
+  );
+});
